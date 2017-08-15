e77818077d9bdb7485de9956c1771d3cbd4faa6e
Lewis Cowles <lewiscowles@me.com>
2017-08-15 09:18:15 +0100

    Added Automated Tests
    * Added tests for config and users
    * Updated Dependencies
    * Fixed typo in `docker-compose.yml`
    * Added test command to `manage.py`

--- project/config.py
+++ project/config.py
@@ -10,7 +10,7 @@ class BaseConfig:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SECRET_KEY = 'my_precious'
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
